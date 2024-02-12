c81072dfef688f95066ac5913ee45f9a4f95e6ba
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2024-02-12 18:44:47 +0530

    Update user authentication logic

---
+++ data/education.js
@@ -0,0 +1,22 @@
+const education = [
+  {
+    degree: "B.Tech, Computer Science",
+    institution: "University Of Engineering & Management, New Town",
+    status: "Currently Pursuing",
+    expectedGraduation: "2025",
+  },
+  {
+    degree: "Senior Secondary (XII), Science",
+    institution: "SHRI RITAM VIDYAPITH (WBCHSE board)",
+    yearOfCompletion: "2021",
+    percentage: "76.60%",
+  },
+  {
+    degree: "Secondary (X)",
+    institution: "Behala Aryya Vidyamandir (WBBSE board)",
+    yearOfCompletion: "2017",
+    percentage: "85.70%",
+  },
+];
+
+export default education;
