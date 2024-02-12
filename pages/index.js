9ee98d8b9e956a880fd382c16aee088c3a501d99
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2024-02-12 17:58:03 +0530

    Update portfolio link in index.js

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ function index() {
     <div>
       <h1>Ankur Halder</h1>
       <h2>
-        <a href="ankurhalder.netlify.app">
+        <a href="https://ankurhalder.netlify.app/" target="__blank">
           Check out my outdated portfolio for now. I am working on a new one. It
           will be live soon.
         </a>
