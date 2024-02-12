7027e1cc2301812a4a995503226302e32008d038
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2024-02-12 12:43:19 +0530

    Update index.js with new content

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
 function index() {
-  return <div>Ankur Halder testing</div>;
+  return (
+    <div>
+      <h1>Ankur Halder</h1>
+      <h2>
+        <a href="ankurhalder.netlify.app">
+          Check out my outdated portfolio for now. I am working on a new one.
+        </a>
+      </h2>
+    </div>
+  );
 }
 
 export default index;
