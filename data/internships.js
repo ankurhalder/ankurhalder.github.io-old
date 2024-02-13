e6c21c75a93bbc3886c68d03774f9f10dec634b4
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2024-02-13 14:35:25 +0530

    Fix bug in login functionality***
    ***Refactor code for better performance***
    
    ***Add new feature for user authentication***
    
    ***Update UI for better user experience

---
+++ data/internships.js
@@ -0,0 +1,14 @@
+const internships = [
+  {
+    title: "Student Intern",
+    company: "CODSOFT",
+    duration: "10/10/2023 - 10/11/2023",
+    type: "Virtual/Work from Home",
+    topic: "Web Development",
+    certificationCredential:
+      "https://drive.google.com/file/d/1UTuhpUyL-AEO9VyOQgXhRHm-IyR-CVvb/view",
+    image: "/work/codsoft.jpg",
+  },
+];
+
+export default internships;
