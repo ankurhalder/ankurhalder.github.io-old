a6f7cbf685084f635d6dbe3720895fc7595284b2
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2024-02-13 14:32:07 +0530

    Refactor code to improve performance and readability

---
+++ data/skills.js
@@ -0,0 +1,60 @@
+const skills = [
+  {
+    category: "Programming Languages",
+    list: ["JavaScript", "Python", "C"],
+  },
+  {
+    category: "Web Development",
+    list: ["React.js", "Next.js", "HTML5", "CSS", "Bootstrap"],
+  },
+  {
+    category: "Frontend Frameworks/Libraries",
+    list: ["Advanced React", "React.js"],
+  },
+  {
+    category: "Backend Frameworks/Libraries",
+    list: ["Express.js", "Node.js", "Django", "Django REST Framework"],
+  },
+  {
+    category: "UI/UX",
+    list: ["UX/UI", "Figma (Software)"],
+  },
+  {
+    category: "Database",
+    list: ["SQL", "NoSQL", "MongoDB", "Mongoose ODM", "PL/SQL"],
+  },
+  {
+    category: "Cloud Technologies",
+    list: ["Google Cloud Dataflow", "Amazon Web Services (AWS)", "IBM Cloud"],
+  },
+  {
+    category: "DevOps/CI/CD",
+    list: [
+      "Cloud Native Practices",
+      "Continuous Integration and Continuous Delivery (CI/CD)",
+      "DevOps",
+      "GitFlow",
+      "Version Control",
+      "Git",
+      "GitHub",
+      "Docker",
+      "Kubernetes",
+      "OpenShift",
+      "Serverless Computing",
+    ],
+  },
+  {
+    category: "Other",
+    list: [
+      "Software Development Life Cycle (SDLC)",
+      "Containers",
+      "Microservices",
+      "Application Security",
+      "Linux",
+      "REST APIs",
+      "Object-Relational Mapping (ORM)",
+    ],
+  },
+];
+
+export default skills;
