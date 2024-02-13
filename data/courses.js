103008c35c60e47bce2386772f3fc46cd50b386f
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2024-02-13 18:35:43 +0530

    Fix bug in login functionality

---
+++ data/courses.js
@@ -0,0 +1,33 @@
+const courses = [
+  {
+    title: "Meta Front-End Developer",
+    provider: "Coursera",
+    completionDate: "Aug 30, 2023",
+    description:
+      "As Ankur Halder, I successfully completed the Meta Front-End Developer program, equipping myself with a comprehensive understanding of front-end development. Through this 9-course program, I gained practical knowledge and hands-on experience in HTML, CSS, JavaScript, React, version control, and UX/UI design principles. With these skills, I am well-prepared to contribute to front-end development projects and deliver user-friendly and visually appealing web applications.",
+    topics: [
+      "Introduction to Front-End Development",
+      "Programming with JavaScript",
+      "Version Control",
+      "HTML and CSS in depth",
+      "React Basics",
+      "Advanced React",
+      "Principles of UX/UI Design",
+      "Front-End Developer Capstone",
+      "Coding Interview Preparation",
+    ],
+    certificateLink:
+      "https://coursera.org/verify/professional-cert/4GVM3WFXRHB6",
+    skillsGained: [
+      "Frontend Development",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "React.js",
+      "Version Control",
+      "UX/UI Design",
+    ],
+  },
+];
+
+export default courses;
