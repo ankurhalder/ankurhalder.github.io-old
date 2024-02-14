e2e8aa9ad503c3a68441d2b23c3e42d1be8635aa
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2024-02-14 11:58:54 +0530

    Add AdComSys 2024 Website and Space Otaku project

--- data/projects.js
+++ data/projects.js
@@ -1,6 +1,7 @@
 const projects = [
   // @ AdComSys 2024 Website
   {
+    logo: "/adcomsys.png",
     title: "AdComSys 2024 Website",
     duration: "May 2023 - Sep 2023",
     organization: "University of Engineering & Management (UEM)",
@@ -38,7 +39,59 @@ const projects = [
       "NextGenLeaders",
     ],
     projectLink: "https://adcomsys.uem.edu.in",
-    logo: "/adcomsys.png",
+  },
+  {
+    logo: "/spaceotaku.png",
+    title: "Space Otaku",
+    duration: "Jan 2023 - Sep 2023",
+    organization: "University of Engineering & Management (UEM)",
+    description:
+      "Space Otaku is your ultimate destination for all things space! Powered by Next.js, Sass, and Dialogflow, it provides a seamless experience for space enthusiasts. Track astronauts, explore the ISS, and dive into celestial wonders!",
+    features: [
+      "Live Astronaut Tracking",
+      "ISS Location",
+      "Satellite Tracker",
+      "Solar System Exploration",
+      "Amazing Space Facts",
+      "Astronauts in Space",
+      "Space Shooter",
+      "Space Quiz",
+    ],
+    navigationLinks: [
+      "Home",
+      "Solar System",
+      "Celestial Bodies",
+      "ISS Tracker",
+      "Amazing Space Facts",
+      "Astronauts in Space",
+      "Space Shooter",
+      "Space Quiz",
+    ],
+    technologies: [
+      "Next.js",
+      "Sass",
+      "Dialogflow",
+      "HTML5 Canvas",
+      "Three.js",
+      "Git",
+      "GitHub",
+      "Responsive Web Design",
+    ],
+    skillsGained: [
+      "Next.js",
+      "React.js",
+      "Advanced React",
+      "Cascading Style Sheets (CSS)",
+      "Dialogflow",
+      "HTML5 Canvas",
+      "Three.js",
+      "Git",
+      "GitHub",
+      "Responsive Web Design",
+    ],
+    githubUrl: "https://github.com/ankurhalder/spaceotaku",
+    projectUrl: "https://www.spaceotaku.online",
+    license: "GNU AFFERO GENERAL PUBLIC LICENSE",
   },
 ];
 
