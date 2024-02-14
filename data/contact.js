9e4dee6c0b4f7579d9fd0fde9d21eb0856e9296b
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2024-02-14 11:47:04 +0530

    Add certificate image to internship data

---
+++ data/contact.js
@@ -0,0 +1,7 @@
+const contact = {
+  email: "ankur.halder12345@gmail.com",
+  phone: "+91 97489 03490",
+  nationality: "Indian",
+};
+
+export default contact;
--- data/internships.js
+++ data/internships.js
@@ -8,6 +8,8 @@ const internships = [
     topic: "Web Development",
     certificationCredential:
       "https://drive.google.com/file/d/1UTuhpUyL-AEO9VyOQgXhRHm-IyR-CVvb/view",
+
+    cetificateImage: "codsoft.jpg",
     description:
       "During my internship at CODSOFT, I had the opportunity to work remotely and gain hands-on experience in web development. I collaborated with the development team to create and enhance web applications, focusing on front-end and back-end development tasks. My responsibilities included implementing new features, troubleshooting issues, and optimizing website performance. Throughout the internship, I deepened my understanding of web technologies such as HTML, CSS, JavaScript, react.js, node.js and mongoDB  and gained practical experience with frameworks and libraries. Additionally, I learned to effectively communicate and collaborate in a virtual work environment, demonstrating adaptability and teamwork.",
   },
