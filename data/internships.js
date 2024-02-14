056fad85b69884e4af3a4170422eaf4be8421a34
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2024-02-14 11:40:56 +0530

    Update internship details in internships.js

--- data/internships.js
+++ data/internships.js
@@ -2,12 +2,14 @@ const internships = [
   {
     title: "Student Intern",
     company: "CODSOFT",
-    duration: "10/10/2023 - 10/11/2023",
-    type: "Virtual/Work from Home",
+    location: "Virtual/Work from Home",
+    startDate: "October 10, 2023",
+    endDate: "October 11, 2023",
     topic: "Web Development",
     certificationCredential:
       "https://drive.google.com/file/d/1UTuhpUyL-AEO9VyOQgXhRHm-IyR-CVvb/view",
-    image: "/work/codsoft.jpg",
+    description:
+      "During my internship at CODSOFT, I had the opportunity to work remotely and gain hands-on experience in web development. I collaborated with the development team to create and enhance web applications, focusing on front-end and back-end development tasks. My responsibilities included implementing new features, troubleshooting issues, and optimizing website performance. Throughout the internship, I deepened my understanding of web technologies such as HTML, CSS, JavaScript, react.js, node.js and mongoDB  and gained practical experience with frameworks and libraries. Additionally, I learned to effectively communicate and collaborate in a virtual work environment, demonstrating adaptability and teamwork.",
   },
 ];
 
