2f37c7c5ae1dd77d87faf2caa0a7e7646b48d4fd
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2024-02-14 11:53:18 +0530

    Fix bug in login functionality

---
+++ data/projects.js
@@ -0,0 +1,45 @@
+const projects = [
+  // @ AdComSys 2024 Website
+  {
+    title: "AdComSys 2024 Website",
+    duration: "May 2023 - Sep 2023",
+    organization: "University of Engineering & Management (UEM)",
+    description:
+      "Developed the official website for the First International Conference on Advanced Computing and Systems, AdComSys 2024, organized by UEM. Collaborated with Vidit Modi to create a seamless digital experience facilitating knowledge exchange and collaboration in cutting-edge technologies.",
+    technologies: [
+      "HTML",
+      "CSS",
+      "SCSS",
+      "JavaScript",
+      "React.js",
+      "Next.js",
+      "Google Dialogflow",
+    ],
+    skillsGained: [
+      "Next.js",
+      "React.js",
+      "Cascading Style Sheets (CSS)",
+      "Advanced React",
+      "Dialogflow",
+    ],
+    website: {
+      production: "adcomsys.uem.edu.in",
+      development: "adcomsys.vercel.app",
+    },
+    eventDate: "June 26th-27th, 2024",
+    additionalInfo:
+      "The website serves as the central hub for the conference, offering a comprehensive overview of the event's objectives, agenda, and speakers.",
+    hashtags: [
+      "AdComSys2024",
+      "UEM",
+      "TechConference",
+      "SoftwareDevelopment",
+      "ProfessionalAchievement",
+      "NextGenLeaders",
+    ],
+    projectLink: "https://adcomsys.uem.edu.in",
+    logo: "/adcomsys.png",
+  },
+];
+
+export default projects;
