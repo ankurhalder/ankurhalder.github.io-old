5a35d0c8209c327523371f5c5d65f7a3177b1441
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2024-02-15 18:25:00 +0530

    Add analytics to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,13 @@
+import { Fragment } from "react";
+import { Analytics } from "@vercel/analytics/react";
 import "../styles/main.scss";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Fragment>
+      <Component {...pageProps} />
+      <Analytics></Analytics>
+    </Fragment>
+  );
 }
 
 export default MyApp;
