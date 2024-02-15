7c952de3862d9957e69e733da07918054398adfa
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2024-02-15 16:27:04 +0530

    Remove Matomo tracking code

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,4 @@
-import React, { useEffect } from "react";
-
 function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    var _mtm = (window._mtm = window._mtm || []);
-    _mtm.push({ "mtm.startTime": new Date().getTime(), event: "mtm.Start" });
-    var d = document,
-      g = d.createElement("script"),
-      s = d.getElementsByTagName("script")[0];
-    g.async = true;
-    g.src =
-      "https://cdn.matomo.cloud/ankurhalder.matomo.cloud/container_1F2GwP8E.js";
-    s.parentNode.insertBefore(g, s);
-  }, []);
-
   return <Component {...pageProps} />;
 }
 
