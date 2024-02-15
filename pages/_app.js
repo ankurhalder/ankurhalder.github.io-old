f454309837c50182261bfeb91257832fba9b5a9b
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2024-02-15 15:58:37 +0530

    added matomo feature

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,19 @@
-export default function App({ Component, pageProps }) {
+import React, { useEffect } from "react";
+
+function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    var _mtm = (window._mtm = window._mtm || []);
+    _mtm.push({ "mtm.startTime": new Date().getTime(), event: "mtm.Start" });
+    var d = document,
+      g = d.createElement("script"),
+      s = d.getElementsByTagName("script")[0];
+    g.async = true;
+    g.src =
+      "https://cdn.matomo.cloud/ankurhalder.matomo.cloud/container_1F2GwP8E.js";
+    s.parentNode.insertBefore(g, s);
+  }, []);
+
   return <Component {...pageProps} />;
 }
+
+export default MyApp;
