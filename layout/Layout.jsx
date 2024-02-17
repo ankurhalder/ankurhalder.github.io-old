56425e7ddc6730773536d89a99ad9e3809266994
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2024-02-17 19:33:04 +0530

    Add links to sidebar

--- layout/Layout.jsx
+++ layout/Layout.jsx
@@ -1,5 +1,6 @@
 import { Fragment, useState } from "react";
 import { Navbar } from "@/container";
+import Link from "next/link";
 
 function Layout({ children }) {
   const [showSidebar, setShowSidebar] = useState(false);
@@ -7,9 +8,25 @@ function Layout({ children }) {
     setShowSidebar(!showSidebar);
   };
 
+  const links = [
+    { href: "/about", label: "about" },
+    { href: "/contact", label: "contact" },
+    { href: "/skills", label: "skills" },
+    { href: "/training", label: "training" },
+  ];
+
   return (
     <Fragment>
-      <div className={`sidebar ${showSidebar ? "show" : ""}`}></div>
+      <div className={`sidebar ${showSidebar ? "show" : ""}`}>
+        <ul>
+          {/* Mapping over the links array to render links */}
+          {links.map((link, index) => (
+            <li key={index}>
+              <Link href={link.href}>{link.label}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
 
       <div className={`mainContent ${showSidebar ? "shift" : ""}`}>
         <Navbar showsidebar={showSidebar} toggleSidebar={toggleSidebar} />
