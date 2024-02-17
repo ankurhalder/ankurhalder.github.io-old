e851079c60b32718aed5557c6e881e3330b664a2
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2024-02-17 15:55:20 +0530

    Refactor code to improve performance and readability

---
+++ pages/skills/index.jsx
@@ -0,0 +1,13 @@
+import { Fragment } from "react";
+import { Layout } from "@/layout";
+function Skills() {
+  return (
+    <Fragment>
+      <Layout>
+        <h1>Skills</h1>
+      </Layout>
+    </Fragment>
+  );
+}
+
+export default Skills;
