0660e8641886df57e8be9edd8694a8ef3376e007
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2024-02-17 15:56:26 +0530

    Refactor code to improve performance and readability

---
+++ pages/contact/index.jsx
@@ -0,0 +1,13 @@
+import { Fragment } from "react";
+import { Layout } from "@/layout";
+function Contact() {
+  return (
+    <Fragment>
+      <Layout>
+        <h1>Contact</h1>
+      </Layout>
+    </Fragment>
+  );
+}
+
+export default Contact;
