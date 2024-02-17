b57eb6c130a300c764ab5a75a5eccb77c39bc013
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2024-02-17 15:57:19 +0530

    Fix bug in login functionality***
    ***Add validation for user input***
    
    ***Refactor code for better performance***
    
    ***Update README with installation instructions***
    
    ***Fix formatting issues in CSS***
    
    ***Add error handling for database connection***
    
    ***Update API endpoint for better security***
    
    ***Remove unused imports***
    
    ***Fix broken unit tests***
    
    ***Update documentation for new feature

---
+++ pages/training/index.jsx
@@ -0,0 +1,13 @@
+import { Fragment } from "react";
+import { Layout } from "@/layout";
+function Training() {
+  return (
+    <Fragment>
+      <Layout>
+        <h1>Training</h1>
+      </Layout>
+    </Fragment>
+  );
+}
+
+export default Training;
