320347f7baf5a199b92dff8386661601043ceac7
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2024-02-17 14:45:51 +0530

    Add sidebar functionality to Layout component

--- layout/Layout.jsx
+++ layout/Layout.jsx
@@ -1,7 +1,33 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 
 function Layout({ children }) {
-  return <Fragment>{children}</Fragment>;
+  const [showSidebar, setShowSidebar] = useState(false);
+
+  const toggleSidebar = () => {
+    setShowSidebar(!showSidebar);
+  };
+
+  return (
+    <Fragment>
+      {/* Hamburger Menu */}
+      <div className="hamburgerMenu" onClick={toggleSidebar}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+      {/* Sidebar */}
+      <div className={`sidebar ${showSidebar ? "show" : ""}`}>
+        {/* Sidebar content goes here */}
+        {/* You can place your sidebar items and links here */}
+      </div>
+
+      {/* Main Content */}
+      <div className={`mainContent ${showSidebar ? "shift" : ""}`}>
+        {children}
+      </div>
+    </Fragment>
+  );
 }
 
 export default Layout;
