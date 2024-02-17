1ffea1f1f9fbc027863a17d58f58bbbfb842153b
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2024-02-17 15:51:32 +0530

    Remove commented out code for hamburger menu

--- layout/Layout.jsx
+++ layout/Layout.jsx
@@ -9,15 +9,6 @@ function Layout({ children }) {
 
   return (
     <Fragment>
-      {/* <div
-        className={`hamburgerMenu ${showSidebar ? "open" : ""}`}
-        onClick={toggleSidebar}
-      >
-        <span></span>
-        <span></span>
-        <span></span>
-      </div> */}
-
       <div className={`sidebar ${showSidebar ? "show" : ""}`}>hi</div>
 
       <div className={`mainContent ${showSidebar ? "shift" : ""}`}>
