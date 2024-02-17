b457ea943292e64d996922b8127ff30269d3d57a
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2024-02-17 13:42:43 +0530

    Refactor layout and import Sidebar component

--- layout/Layout.jsx
+++ layout/Layout.jsx
@@ -1,29 +1,11 @@
-import { Fragment, useState } from "react";
+import { Fragment } from "react";
+import { Sidebar } from "@/container";
 
 function Layout({ children }) {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
-
   return (
     <Fragment>
-      <div
-        className={`hamburgerMenu ${isMenuOpen ? "open" : ""}`}
-        onClick={toggleMenu}
-      >
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
-
-      <div className={`content ${isMenuOpen ? "bent" : ""}`}>{children}</div>
-
-      <div
-        className={`sideNavbar ${isMenuOpen ? "open" : "closed"}`}
-        onClick={toggleMenu}
-      ></div>
+      <Sidebar></Sidebar>
+      {children}
     </Fragment>
   );
 }
