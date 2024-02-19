e12a3d40051fe837359b9fb691ac31b100bbee05
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2024-02-19 16:54:25 +0530

    Fix console error in production

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,9 @@ import { Fragment } from "react";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/react";
 import "../styles/main.scss";
-// if (process.env.NODE_ENV === "production") {
-//   console.error = () => {};
-// }
+if (process.env.NODE_ENV === "production") {
+  console.error = () => {};
+}
 function MyApp({ Component, pageProps }) {
   return (
     <Fragment>
