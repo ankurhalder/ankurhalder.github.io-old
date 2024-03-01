d7ab112769f1bd58c96231a7f8c5fde242a20f03
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2024-03-01 15:22:28 +0530

    Update website URL in personalInfo.js

--- data/personalInfo.js
+++ data/personalInfo.js
@@ -7,7 +7,7 @@ const personalInfo = {
   languagesKnown: ["Bengali", "Hindi", "English"],
   address: "13R Ishan Ghosh Road, Kolkata: 700008, West Bengal, India",
   email: "ankur.halder12345@gmail.com",
-  website: "https://ankurhalder.github.io",
+  website: "ankurhalder.github.in",
   imageUrl: "/ankurhalder.jpg",
   about:
     "Hi 👋, I'm Ankur Halder. Passionate and enthusiastic about software development, proficient in MERN stack, Django (full stack, REST API, ORM), Python (OOP), JavaScript (OOP), Next.js (full stack), and DSA. Confident and dedicated in creating effective solutions. Moreover, active contributor to open-source projects.",
