3cfd1fcafa82e602b34af6bdc005e770de5b60a1
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2024-03-16 19:09:44 +0530

    Remove unnecessary comments in CheckboxComponent.jsx

--- components/CheckboxComponent.jsx
+++ components/CheckboxComponent.jsx
@@ -15,8 +15,6 @@ function CheckboxComponent() {
     document.body.classList.toggle("theme-dark");
     document.body.classList.toggle("theme-light");
   };
-  //
-  //
   return (
     <div className={`checkbox ${isChecked ? "checked" : ""}`}>
       <input
